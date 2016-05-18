90fcd268c535d5b640b43d381fc04f8b6698a29c
lyndon <lyndon@moneyball.io>
2016-05-18 10:55:58 +0800

    fix index info

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello env-production!');
+  res.send('Hello env-productionNo2!');
 });
 
 var server = app.listen(3000, function () {
